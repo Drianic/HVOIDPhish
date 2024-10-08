b37db8ca090015e7f9fee2d22664726224c40b19
Dorian <73549329+Drianic@users.noreply.github.com>
2024-10-08 22:35:46 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo 'instalation of HVOIDPHISH'|lolcat
+pip install colorama
+pip install Flask
+pip install os
+pip install time
+pip install sys
+apt install lolcat
+apt install figlet
+apt install sl
