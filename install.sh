af30e4879b2a3ba28aac5ffe58447a619d733c65
Dorian <73549329+Drianic@users.noreply.github.com>
2024-10-08 18:40:45 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo 'instalation of HVOIDPHISH'|lolcat
-pip install colorama
-pip install Flask
-pip install os
-pip install time
-pip install sys
-apt install lolcat
-apt install figlet
-apt install sl
